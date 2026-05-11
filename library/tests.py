f641a5fcdd5d6b2d9581727cd6056af61cb634a4
vallal <vallalperumalvallal@gmail.com>
2026-05-11 15:12:03 +0530

    Added user register test view

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import User
 # Create your tests here.
+
+class TestUserRegisterView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='test123', email='test@gmail.com')
+
+
+    def test_user_created(self):
+        self.assertEqual(self.user.username, 'test_user')
+        self.assertEqual(self.user.email, 'test@gmail.com')
